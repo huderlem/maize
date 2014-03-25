b3dbf655dc188d10d8cd6bafec9d9b4642e02782
Marcus Huderle <huderlem@gmail.com>
2014-03-25 14:23:45 -0500

    Changed Route 1 wild data.

--- main.asm
+++ main.asm
@@ -20778,16 +20778,16 @@ NoMons: ; d0dd (3:50dd)
 
 Route1Mons: ; d0df (3:50df)
 	db $19
-	db 3,PIDGEY
 	db 3,RATTATA
-	db 3,RATTATA
-	db 2,RATTATA
-	db 2,PIDGEY
-	db 3,PIDGEY
-	db 3,PIDGEY
+	db 3,SPEAROW
+	db 4,SPEAROW
 	db 4,RATTATA
-	db 4,PIDGEY
-	db 5,PIDGEY
+	db 2,SPEAROW
+	db 5,SPEAROW
+	db 3,PIDGEY
+	db 5,RATTATA
+	db 5,NIDORAN_M
+	db 5,NIDORAN_F
 
 	db $00
 
