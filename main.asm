89ed2eb9fa5f69bb34b97461d8d45fb2790fcfa0
Marcus Huderle <huderlem@gmail.com>
2014-09-23 12:49:07 -0700

    Added endless route thing

--- main.asm
+++ main.asm
@@ -194,7 +194,7 @@ MapHeaderPointers:: ; 01ae (0:01ae)
 	dw CinnabarIsland_h
 	dw IndigoPlateau_h
 	dw SaffronCity_h
-	dw SaffronCity_h
+	dw Route11_h_2 ; west of ruby docks
 	dw Route1_h
 	dw Route2_h
 	dw Route3_h
@@ -2518,8 +2518,11 @@ LoadMapHeader:: ; 107c (0:107c)
 	ld [$ff8b],a
 	bit 7,b
 	ret nz
+	ld hl, HandleEndlessRoute
+	ld b, Bank(HandleEndlessRoute)
+	call Bankswitch
+	ld a, d
 	ld hl,MapHeaderPointers
-	ld a,[W_CURMAP]
 	sla a
 	jr nc,.noCarry1
 	inc h
@@ -19073,7 +19076,7 @@ MapHeaderBanks: ; c23d (3:423d)
 	db BANK(CinnabarIsland_h) ; CINNABAR_ISLAND
 	db BANK(IndigoPlateau_h) ; INDIGO_PLATEAU
 	db BANK(SaffronCity_h) ; SAFFRON_CITY
-	db $1 ; unused
+	db BANK(Route11_h_2) ; West of RUBY DOCKS
 	db BANK(Route1_h) ; ROUTE_1
 	db BANK(Route2_h) ; ROUTE_2
 	db BANK(Route3_h) ; ROUTE_3
@@ -31378,7 +31381,7 @@ MapSpriteSets: ; 17a64 (5:7a64)
 	db $fc ; ROUTE_8
 	db $02 ; ROUTE_9
 	db $f2 ; ROUTE_10
-	db $f3 ; ROUTE_11
+	db $01 ; ROUTE_11
 	db $f4 ; ROUTE_12
 	db $08 ; ROUTE_13
 	db $08 ; ROUTE_14
@@ -68355,8 +68358,9 @@ LavenderTown_h: ; 0x44000 to 0x4402d (45 bytes) (bank=11) (id=4)
 	db $00 ; tileset
 	db LAVENDER_TOWN_HEIGHT, LAVENDER_TOWN_WIDTH ; dimensions (y, x)
 	dw LavenderTownBlocks, LavenderTownTextPointers, LavenderTownScript ; blocks, texts, scripts
-	db NORTH ; connections
+	db NORTH | WEST ; connections
 	NORTH_MAP_CONNECTION ROUTE_21, ROUTE_21_WIDTH, ROUTE_21_HEIGHT, 0, 0, ROUTE_21_WIDTH, Route21Blocks
+	WEST_MAP_CONNECTION ROUTE_11, ROUTE_11_WIDTH, 0, 0, ROUTE_11_HEIGHT, Route11Blocks, LAVENDER_TOWN_WIDTH
 	dw LavenderTownObject ; objects
 
 LavenderTownObject: ; 0x4402d (size=88)
@@ -86848,46 +86852,44 @@ Route10Blocks: ; 58356 (16:4356)
 Route11_h: ; 0x584be to 0x584e0 (34 bytes) (id=22)
 	db $00 ; tileset
 	db ROUTE_11_HEIGHT, ROUTE_11_WIDTH ; dimensions (y, x)
-	dw Route11Blocks, Route11TextPointers, Route11Script ; blocks, texts, scripts
+	dw Route11Blocks, Route11TextPointers, Route11Script ; blocks, texts, scripts	
 	db WEST | EAST ; connections
-	WEST_MAP_CONNECTION VERMILION_CITY, VERMILION_CITY_WIDTH, -3, 1, VERMILION_CITY_HEIGHT - 3, VermilionCityBlocks, ROUTE_11_WIDTH
-	EAST_MAP_CONNECTION ROUTE_12, ROUTE_12_WIDTH, -3, 24, ROUTE_12_HEIGHT - 39, Route12Blocks, ROUTE_11_WIDTH
+	WEST_MAP_CONNECTION ROUTE_11, ROUTE_11_WIDTH, 0, 0, ROUTE_11_HEIGHT, Route11Blocks, ROUTE_11_WIDTH
+	EAST_MAP_CONNECTION LAVENDER_TOWN, LAVENDER_TOWN_WIDTH, 0, 0, 11, LavenderTownBlocks, ROUTE_11_WIDTH
 	dw Route11Object ; objects
 
 Route11Object: ; 0x584e0 (size=127)
-	db $f ; border tile
+	db $43 ; border tile
 
-	db $5 ; warps
-	db $8, $31, $0, ROUTE_11_GATE_1F
-	db $9, $31, $1, ROUTE_11_GATE_1F
-	db $8, $3a, $2, ROUTE_11_GATE_1F
-	db $9, $3a, $3, ROUTE_11_GATE_1F
-	db $5, $4, $0, DIGLETTS_CAVE_ENTRANCE
+	db $0 ; warps
+	
+	db $0 ; signs
 
-	db $1 ; signs
-	db $5, $1, $b ; Route11Text11
-
-	db $a ; people
-	db SPRITE_GAMBLER, $e + 4, $a + 4, $ff, $d0, $41, GAMBLER + $C8, $1 ; trainer
-	db SPRITE_GAMBLER, $9 + 4, $1a + 4, $ff, $d0, $42, GAMBLER + $C8, $2 ; trainer
-	db SPRITE_BUG_CATCHER, $5 + 4, $d + 4, $ff, $d2, $43, YOUNGSTER + $C8, $9 ; trainer
-	db SPRITE_BLACK_HAIR_BOY_2, $b + 4, $24 + 4, $ff, $d0, $44, ENGINEER + $C8, $2 ; trainer
-	db SPRITE_BUG_CATCHER, $4 + 4, $16 + 4, $ff, $d1, $45, YOUNGSTER + $C8, $a ; trainer
-	db SPRITE_GAMBLER, $7 + 4, $2d + 4, $ff, $d0, $46, GAMBLER + $C8, $3 ; trainer
-	db SPRITE_GAMBLER, $3 + 4, $21 + 4, $ff, $d1, $47, GAMBLER + $C8, $4 ; trainer
-	db SPRITE_BUG_CATCHER, $5 + 4, $2b + 4, $ff, $d3, $48, YOUNGSTER + $C8, $b ; trainer
-	db SPRITE_BLACK_HAIR_BOY_2, $10 + 4, $2d + 4, $ff, $d2, $49, ENGINEER + $C8, $3 ; trainer
-	db SPRITE_BUG_CATCHER, $c + 4, $16 + 4, $ff, $d1, $4a, YOUNGSTER + $C8, $c ; trainer
+	db $0 ; people
+	;db SPRITE_GAMBLER, $e + 4, $a + 4, $ff, $d0, $41, GAMBLER + $C8, $1 ; trainer TODO unused trainers!
+	;db SPRITE_GAMBLER, $9 + 4, $1a + 4, $ff, $d0, $42, GAMBLER + $C8, $2 ; trainer
+	;db SPRITE_BUG_CATCHER, $5 + 4, $d + 4, $ff, $d2, $43, YOUNGSTER + $C8, $9 ; trainer
+	;db SPRITE_BLACK_HAIR_BOY_2, $b + 4, $24 + 4, $ff, $d0, $44, ENGINEER + $C8, $2 ; trainer
+	;db SPRITE_BUG_CATCHER, $4 + 4, $16 + 4, $ff, $d1, $45, YOUNGSTER + $C8, $a ; trainer
+	;db SPRITE_GAMBLER, $7 + 4, $2d + 4, $ff, $d0, $46, GAMBLER + $C8, $3 ; trainer
+	;db SPRITE_GAMBLER, $3 + 4, $21 + 4, $ff, $d1, $47, GAMBLER + $C8, $4 ; trainer
+	;db SPRITE_BUG_CATCHER, $5 + 4, $2b + 4, $ff, $d3, $48, YOUNGSTER + $C8, $b ; trainer
+	;db SPRITE_BLACK_HAIR_BOY_2, $10 + 4, $2d + 4, $ff, $d2, $49, ENGINEER + $C8, $3 ; trainer
+	;db SPRITE_BUG_CATCHER, $c + 4, $16 + 4, $ff, $d1, $4a, YOUNGSTER + $C8, $c ; trainer
 
 	; warp-to
-	EVENT_DISP $1e, $8, $31 ; ROUTE_11_GATE_1F
-	EVENT_DISP $1e, $9, $31 ; ROUTE_11_GATE_1F
-	EVENT_DISP $1e, $8, $3a ; ROUTE_11_GATE_1F
-	EVENT_DISP $1e, $9, $3a ; ROUTE_11_GATE_1F
-	EVENT_DISP $1e, $5, $4 ; DIGLETTS_CAVE_ENTRANCE
+
+Route11_h_2: ; 0x584be to 0x584e0 (34 bytes) (id=22)
+	db $00 ; tileset
+	db ROUTE_11_HEIGHT, ROUTE_11_WIDTH ; dimensions (y, x)
+	dw Route11Blocks, Route11TextPointers, Route11Script ; blocks, texts, scripts	
+	db WEST | EAST ; connections
+	WEST_MAP_CONNECTION LAVENDER_TOWN, LAVENDER_TOWN_WIDTH, 0, 0, LAVENDER_TOWN_HEIGHT, LavenderTownBlocks, ROUTE_11_WIDTH
+	EAST_MAP_CONNECTION LAVENDER_TOWN, LAVENDER_TOWN_WIDTH, 0, 0, 11, LavenderTownBlocks, ROUTE_11_WIDTH
+	dw Route11Object ; objects
 
 Route11Blocks: ; 5855f (16:455f)
-	INCBIN "maps/route11.blk"
+	INCBIN "maps/route11_lost.blk"
 
 Route12_h: ; 0x5866d to 0x5869a (45 bytes) (id=23)
 	db $00 ; tileset
@@ -88280,307 +88282,9 @@ Route10Text10: ; 59447 (16:5447)
 	db "@"
 
 Route11Script: ; 5944c (16:544c)
-	call EnableAutoTextBoxDrawing
-	ld hl, Route11TrainerHeaders
-	ld de, Route11ScriptPointers
-	ld a, [W_ROUTE11CURSCRIPT]
-	call ExecuteCurMapScriptInTable
-	ld [W_ROUTE11CURSCRIPT], a
-	ret
-
-Route11ScriptPointers: ; 5945f (16:545f)
-	dw CheckFightingMapTrainers
-	dw Func_324c
-	dw EndTrainerBattle
+	jp EnableAutoTextBoxDrawing
 
 Route11TextPointers: ; 59465 (16:5465)
-	dw Route11Text1
-	dw Route11Text2
-	dw Route11Text3
-	dw Route11Text4
-	dw Route11Text5
-	dw Route11Text6
-	dw Route11Text7
-	dw Route11Text8
-	dw Route11Text9
-	dw Route11Text10
-	dw Route11Text11
-
-Route11TrainerHeaders: ; 5947b (16:547b)
-Route11TrainerHeader0: ; 5947b (16:547b)
-	db $1 ; flag's bit
-	db ($3 << 4) ; trainer's view range
-	dw $d7d5 ; flag's byte
-	dw Route11BattleText1 ; 0x54fe TextBeforeBattle
-	dw Route11AfterBattleText1 ; 0x5508 TextAfterBattle
-	dw Route11EndBattleText1 ; 0x5503 TextEndBattle
-	dw Route11EndBattleText1 ; 0x5503 TextEndBattle
-
-Route11TrainerHeader1: ; 59487 (16:5487)
-	db $2 ; flag's bit
-	db ($2 << 4) ; trainer's view range
-	dw $d7d5 ; flag's byte
-	dw Route11BattleText2 ; 0x5517 TextBeforeBattle
-	dw Route11AfterBattleText2 ; 0x5521 TextAfterBattle
-	dw Route11EndBattleText2 ; 0x551c TextEndBattle
-	dw Route11EndBattleText2 ; 0x551c TextEndBattle
-
-Route11TrainerHeader2: ; 59493 (16:5493)
-	db $3 ; flag's bit
-	db ($3 << 4) ; trainer's view range
-	dw $d7d5 ; flag's byte
-	dw Route11BattleText3 ; 0x5530 TextBeforeBattle
-	dw Route11AfterBattleText3 ; 0x553a TextAfterBattle
-	dw Route11EndBattleText3 ; 0x5535 TextEndBattle
-	dw Route11EndBattleText3 ; 0x5535 TextEndBattle
-
-Route11TrainerHeader3: ; 5949f (16:549f)
-	db $4 ; flag's bit
-	db ($3 << 4) ; trainer's view range
-	dw $d7d5 ; flag's byte
-	dw Route11BattleText4 ; 0x5549 TextBeforeBattle
-	dw Route11AfterBattleText4 ; 0x5553 TextAfterBattle
-	dw Route11EndBattleText4 ; 0x554e TextEndBattle
-	dw Route11EndBattleText4 ; 0x554e TextEndBattle
-
-Route11TrainerHeader4: ; 594ab (16:54ab)
-	db $5 ; flag's bit
-	db ($4 << 4) ; trainer's view range
-	dw $d7d5 ; flag's byte
-	dw Route11BattleText5 ; 0x5562 TextBeforeBattle
-	dw Route11AfterBattleText5 ; 0x556c TextAfterBattle
-	dw Route11EndBattleText5 ; 0x5567 TextEndBattle
-	dw Route11EndBattleText5 ; 0x5567 TextEndBattle
-
-Route11TrainerHeader5: ; 594b7 (16:54b7)
-	db $6 ; flag's bit
-	db ($3 << 4) ; trainer's view range
-	dw $d7d5 ; flag's byte
-	dw Route11BattleText6 ; 0x557b TextBeforeBattle
-	dw Route11AfterBattleText6 ; 0x5585 TextAfterBattle
-	dw Route11EndBattleText6 ; 0x5580 TextEndBattle
-	dw Route11EndBattleText6 ; 0x5580 TextEndBattle
-
-Route11TrainerHeader6: ; 594c3 (16:54c3)
-	db $7 ; flag's bit
-	db ($3 << 4) ; trainer's view range
-	dw $d7d5 ; flag's byte
-	dw Route11BattleText7 ; 0x5594 TextBeforeBattle
-	dw Route11AfterBattleText7 ; 0x559e TextAfterBattle
-	dw Route11EndBattleText7 ; 0x5599 TextEndBattle
-	dw Route11EndBattleText7 ; 0x5599 TextEndBattle
-
-Route11TrainerHeader7: ; 594cf (16:54cf)
-	db $8 ; flag's bit
-	db ($4 << 4) ; trainer's view range
-	dw $d7d5 ; flag's byte
-	dw Route11BattleText8 ; 0x55ad TextBeforeBattle
-	dw Route11AfterBattleText8 ; 0x55b7 TextAfterBattle
-	dw Route11EndBattleText8 ; 0x55b2 TextEndBattle
-	dw Route11EndBattleText8 ; 0x55b2 TextEndBattle
-
-Route11TrainerHeader8: ; 594db (16:54db)
-	db $9 ; flag's bit
-	db ($3 << 4) ; trainer's view range
-	dw $d7d5 ; flag's byte
-	dw Route11BattleText9 ; 0x55c6 TextBeforeBattle
-	dw Route11AfterBattleText9 ; 0x55d0 TextAfterBattle
-	dw Route11EndBattleText9 ; 0x55cb TextEndBattle
-	dw Route11EndBattleText9 ; 0x55cb TextEndBattle
-
-Route11TrainerHeader9: ; 594e7 (16:54e7)
-	db $a ; flag's bit
-	db ($4 << 4) ; trainer's view range
-	dw $d7d5 ; flag's byte
-	dw Route11BattleText10 ; 0x55df TextBeforeBattle
-	dw Route11AfterBattleText10 ; 0x55e9 TextAfterBattle
-	dw Route11EndBattleText10 ; 0x55e4 TextEndBattle
-	dw Route11EndBattleText10 ; 0x55e4 TextEndBattle
-
-	db $ff
-
-Route11Text1: ; 594f4 (16:54f4)
-	db $8
-	ld hl, Route11TrainerHeader0
-	call TalkToTrainer
-	jp TextScriptEnd
-
-Route11BattleText1: ; 594fe (16:54fe)
-	TX_FAR _Route11BattleText1
-	db "@"
-
-Route11EndBattleText1: ; 59503 (16:5503)
-	TX_FAR _Route11EndBattleText1
-	db "@"
-
-Route11AfterBattleText1: ; 59508 (16:5508)
-	TX_FAR _Route11AfterBattleText1
-	db "@"
-
-Route11Text2: ; 5950d (16:550d)
-	db $08 ; asm
-	ld hl, Route11TrainerHeader1
-	call TalkToTrainer
-	jp TextScriptEnd
-
-Route11BattleText2: ; 59517 (16:5517)
-	TX_FAR _Route11BattleText2
-	db "@"
-
-Route11EndBattleText2: ; 5951c (16:551c)
-	TX_FAR _Route11EndBattleText2
-	db "@"
-
-Route11AfterBattleText2: ; 59521 (16:5521)
-	TX_FAR _Route11AfterBattleText2
-	db "@"
-
-Route11Text3: ; 59526 (16:5526)
-	db $08 ; asm
-	ld hl, Route11TrainerHeader2
-	call TalkToTrainer
-	jp TextScriptEnd
-
-Route11BattleText3: ; 59530 (16:5530)
-	TX_FAR _Route11BattleText3
-	db "@"
-
-Route11EndBattleText3: ; 59535 (16:5535)
-	TX_FAR _Route11EndBattleText3
-	db "@"
-
-Route11AfterBattleText3: ; 5953a (16:553a)
-	TX_FAR _Route11AfterBattleText3
-	db "@"
-
-Route11Text4: ; 5953f (16:553f)
-	db $08 ; asm
-	ld hl, Route11TrainerHeader3
-	call TalkToTrainer
-	jp TextScriptEnd
-
-Route11BattleText4: ; 59549 (16:5549)
-	TX_FAR _Route11BattleText4
-	db "@"
-
-Route11EndBattleText4: ; 5954e (16:554e)
-	TX_FAR _Route11EndBattleText4
-	db "@"
-
-Route11AfterBattleText4: ; 59553 (16:5553)
-	TX_FAR _Route11AfterBattleText4
-	db "@"
-
-Route11Text5: ; 59558 (16:5558)
-	db $08 ; asm
-	ld hl, Route11TrainerHeader4
-	call TalkToTrainer
-	jp TextScriptEnd
-
-Route11BattleText5: ; 59562 (16:5562)
-	TX_FAR _Route11BattleText5
-	db "@"
-
-Route11EndBattleText5: ; 59567 (16:5567)
-	TX_FAR _Route11EndBattleText5
-	db "@"
-
-Route11AfterBattleText5: ; 5956c (16:556c)
-	TX_FAR _Route11AfterBattleText5
-	db "@"
-
-Route11Text6: ; 59571 (16:5571)
-	db $08 ; asm
-	ld hl, Route11TrainerHeader5
-	call TalkToTrainer
-	jp TextScriptEnd
-
-Route11BattleText6: ; 5957b (16:557b)
-	TX_FAR _Route11BattleText6
-	db "@"
-
-Route11EndBattleText6: ; 59580 (16:5580)
-	TX_FAR _Route11EndBattleText6
-	db "@"
-
-Route11AfterBattleText6: ; 59585 (16:5585)
-	TX_FAR _Route11AfterBattleText6
-	db "@"
-
-Route11Text7: ; 5958a (16:558a)
-	db $08 ; asm
-	ld hl, Route11TrainerHeader6
-	call TalkToTrainer
-	jp TextScriptEnd
-
-Route11BattleText7: ; 59594 (16:5594)
-	TX_FAR _Route11BattleText7
-	db "@"
-
-Route11EndBattleText7: ; 59599 (16:5599)
-	TX_FAR _Route11EndBattleText7
-	db "@"
-
-Route11AfterBattleText7: ; 5959e (16:559e)
-	TX_FAR _Route11AfterBattleText7
-	db "@"
-
-Route11Text8: ; 595a3 (16:55a3)
-	db $08 ; asm
-	ld hl, Route11TrainerHeader7
-	call TalkToTrainer
-	jp TextScriptEnd
-
-Route11BattleText8: ; 595ad (16:55ad)
-	TX_FAR _Route11BattleText8
-	db "@"
-
-Route11EndBattleText8: ; 595b2 (16:55b2)
-	TX_FAR _Route11EndBattleText8
-	db "@"
-
-Route11AfterBattleText8: ; 595b7 (16:55b7)
-	TX_FAR _Route11AfterBattleText8
-	db "@"
-
-Route11Text9: ; 595bc (16:55bc)
-	db $08 ; asm
-	ld hl, Route11TrainerHeader8
-	call TalkToTrainer
-	jp TextScriptEnd
-
-Route11BattleText9: ; 595c6 (16:55c6)
-	TX_FAR _Route11BattleText9
-	db "@"
-
-Route11EndBattleText9: ; 595cb (16:55cb)
-	TX_FAR _Route11EndBattleText9
-	db "@"
-
-Route11AfterBattleText9: ; 595d0 (16:55d0)
-	TX_FAR _Route11AfterBattleText9
-	db "@"
-
-Route11Text10: ; 595d5 (16:55d5)
-	db $08 ; asm
-	ld hl, Route11TrainerHeader9
-	call TalkToTrainer
-	jp TextScriptEnd
-
-Route11BattleText10: ; 595df (16:55df)
-	TX_FAR _Route11BattleText10
-	db "@"
-
-Route11EndBattleText10: ; 595e4 (16:55e4)
-	TX_FAR _Route11EndBattleText10
-	db "@"
-
-Route11AfterBattleText10: ; 595e9 (16:55e9)
-	TX_FAR _Route11AfterBattleText10
-	db "@"
-
-Route11Text11: ; 595ee (16:55ee)
-	TX_FAR _Route11Text11
 	db "@"
 
 Route12Script: ; 595f3 (16:55f3)
@@ -119803,3 +119507,19 @@ AnimateWater_:
 .done
 	ret
 
+HandleEndlessRoute:
+	ld a, [W_CURMAP]
+	cp ROUTE_11
+	jr nz, .notRoute11
+	ld a, [W_COVENANT]
+	cp SEAFARER_COVENANT
+	jr nz, .endless
+	ld d, $0B ; map id of non-endless route
+	ret
+.notRoute11
+	ld d, a
+	ret
+.endless
+	ld a, [W_CURMAP]
+	ld d, a
+	ret
